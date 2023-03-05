6711e438857c79d5d5e565a9d2f2782506928b17
zeroleo12345 <zeroleo12345@163.com>
2023-03-05 20:31:57 +0800

    add

---
+++ container/debug.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+export ENVIRONMENT=unittest; export DEBUG=False; ENTRYPOINT=sh docker-compose up -d snmp
---
+++ container/release.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+export ENVIRONMENT=production; export DEBUG=False; export RESTART_POLICY=unless-stopped; ENTRYPOINT="" docker-compose up -d snmp
