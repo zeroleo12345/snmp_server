b3ebeb18ff3f5ab582c85044f3f7f15fb2a77d92
zeroleo12345 <zeroleo12345@163.com>
2023-10-04 10:31:51 +0800

    m

--- src/metric_server.py
+++ src/metric_server.py
@@ -1,35 +1,18 @@
-"""
-ac配置参考:
-    https://www.sohu.com/a/496535833_99906077
-    https://zhiliao.h3c.com/questions/dispcont/232042
-    https://juejin.cn/post/6988416254646157320
-"""
-
 # 导入高层 API
-from pysnmp.hlapi import SnmpEngine, CommunityData, UsmUserData, usmHMACMD5AuthProtocol, \
-    usmAesCfb128Protocol, UdpTransportTarget, ContextData, ObjectIdentity, ObjectType, \
-    getCmd, nextCmd
-from settings import log, COMMUNITY_NAME
+from pysnmp.hlapi import *
 
 
-class Human2(object):
-    oid_map = {
-        'sysName': '1.3.6.1.2.1.1.5.0',
-        'ifOperStatus': '1.3.6.1.2.1.2.2.1.8',
-    }
-
-    def __int__(self):
+class Human(object):
+    def __init__(self):
         # 初始化引擎
         self.engine = SnmpEngine()
+        # 选择 SNMP 协议，v1 和 v2c 只用团体字，使用 CommunityData 类实例化
 
-        # 选择 SNMP 协议:
-
-        # SNMP v1 和 SNMP v2c 采用团体名认证, 使用 CommunityData 类实例化
         # SNMPv1
-        # self.communityData = CommunityData('public', mpModel=0)
+        # communityData = CommunityData('public', mpModel=0)
 
         # SNMPv2c
-        self.communityData = CommunityData(COMMUNITY_NAME, mpModel=1)
+        self.communityData = CommunityData('zhoulixin', mpModel=1)
 
         # SNMPv3, 则需要用户凭证, 使用 UsmUserData 类实例化, 认证和加密算法与上面设备配置相对应
         self.userData = UsmUserData(
@@ -41,21 +24,18 @@ class Human2(object):
         )
 
         # 配置目标主机
-        # TODO
-        ip_port = ('119.131.148.169', 161)
+        ip_port = ('192.168.11.11', 161)
         self.target = UdpTransportTarget(ip_port)
         # 实例化上下文对象
         self.context = ContextData()
-        print(1111111111)
-
-    def get(self, metric_name):
-        log.info(f'get metric: {metric_name}')
+        
+    def getSysName(self):
         # ObjectIdentity 类负责 MIB 对象的识别:
 
         # 方法1: 指定要查询的 OID 对象或名称
-        _id = self.oid_map[metric_name]
-        oid = ObjectIdentity(id)
-
+        _id = '1.3.6.1.2.1.1.5.0'
+        oid = ObjectIdentity(_id)
+        
         # 方法2: 通过oid名字查询
         #  oid = ObjectIdentity('SNMPv2-MIB', 'sysName', 0)
 
@@ -63,7 +43,6 @@ class Human2(object):
         obj = ObjectType(oid)
         # 使用 getCMD 方法进行查询，返回结果是一个迭代器, 需要使用 next() 来取值
         # 传递的参数均为为上面定义的变量, 以 v2c 为例(如果是 v3，communityData 替换为 userData)
-        from pprint import pprint; import pdb; pdb.set_trace()
         g = getCmd(self.engine, self.communityData, self.target, self.context, obj)
 
         # 取值
@@ -71,17 +50,17 @@ class Human2(object):
 
         # 打印输出
         for i in result:
-            log.info(i)
+            print(i)
 
-    def get_all(self, metric_name):
+
+    def getIfaceList(self,):
         """
         这个函数是查询接口列表, 和上面查询 sysName 的区别是使用了 nextCmd 来获取一个 MIB 子树的全部内容
         主要是 `lexicographicMode=False` 参数, 默认为 `True`, 会一直查询到 MIB 树结束.
         """
-        log.info(f'get metric: {metric_name}')
         # 方法1: 指定要查询的 OID 对象或名称
-        _id = self.oid_map[metric_name]
-        oid = ObjectIdentity(id)
+        _id = '1.3.6.1.2.1.2.2.1.8'
+        oid = ObjectIdentity(_id)
 
         # 方法2: 通过oid名字查询
         #  oid = ObjectIdentity('SNMPv2-MIB', 'ifDescr')
@@ -97,85 +76,18 @@ class Human2(object):
                 #  (Pdb) errorStatus.namedValues.getName('noError')
                 #  (Pdb) errorStatus.namedValues.getName(0)
                 if str(errorStatus) != 'noError':
-                    log.error(errorIndication)
-                    log.error(errorStatus)
-                    log.error(errorIndex)
+                    print(errorIndication)
+                    print(errorStatus)
+                    print(errorIndex)
                     continue
                 for iface in varBinds:
-                    log.info(iface)
+                    print(iface)
         except StopIteration:
-            log.info('Get interface list done.')
-
-
-class Human(object):
-    oid_map = {
-        'sysName': '1.3.6.1.2.1.1.5.0',
-        'ifOperStatus': '1.3.6.1.2.1.2.2.1.8',
-    }
-
-    def __init__(self):
-        # 初始化引擎
-        self.engine = SnmpEngine()
-
-        # 选择 SNMP 协议:
-
-        # SNMP v1 和 SNMP v2c 采用团体名认证, 使用 CommunityData 类实例化
-        # SNMPv1
-        # self.communityData = CommunityData('public', mpModel=0)
-
-        # SNMPv2c
-        self.communityData = CommunityData(COMMUNITY_NAME, mpModel=1)
-
-        # SNMPv3, 则需要用户凭证, 使用 UsmUserData 类实例化, 认证和加密算法与上面设备配置相对应
-        self.userData = UsmUserData(
-            userName='admin',
-            authKey='Admin@h3c',
-            privKey='Admin@h3c',
-            authProtocol=usmHMACMD5AuthProtocol,
-            privProtocol=usmAesCfb128Protocol,
-        )
-
-        # 配置目标主机
-        # TODO
-        ip_port = ('119.131.148.169', 161)
-        self.target = UdpTransportTarget(ip_port)
-        # 实例化上下文对象
-        self.context = ContextData()
-        print('Hi，我是 Human 类的初始化方法')
-
-    def get(self, metric_name):
-        log.info(f'get metric: {metric_name}')
-        # ObjectIdentity 类负责 MIB 对象的识别:
-
-        # 方法1: 指定要查询的 OID 对象或名称
-        _id = self.oid_map[metric_name]
-        oid = ObjectIdentity(id)
-
-        # 方法2: 通过oid名字查询
-        #  oid = ObjectIdentity('SNMPv2-MIB', 'sysName', 0)
-
-        # 使用 ObjectType 类初始化查询对象:
-        obj = ObjectType(oid)
-        # 使用 getCMD 方法进行查询，返回结果是一个迭代器, 需要使用 next() 来取值
-        # 传递的参数均为为上面定义的变量, 以 v2c 为例(如果是 v3，communityData 替换为 userData)
-        g = getCmd(self.engine, self.communityData, self.target, self.context, obj)
-
-        # 取值
-        errorIndication, errorStatus, errorIndex, result = next(g)
-        if str(errorStatus) != 'noError':
-            log.error(errorIndication)
-            log.error(errorStatus)
-            log.error(errorIndex)
-            return
-        # 打印输出
-        for i in result:
-            log.info(i)
+            print('Get interface list done.')
 
 
 if __name__ == "__main__":
-    # get sysName
     mib = Human()
-    # mib = Human2()
-    mib.get(metric_name='sysName')
-    # log.info('============================')
-    # mib.get_all(metric_name='ifOperStatus')
+    mib.getSysName()
+    print('============================')
+    mib.getIfaceList()
