114a78a0b8ebf04fe20ddb89f20592b779bd2aec
zeroleo12345 <zeroleo12345@163.com>
2023-10-04 10:11:22 +0800

    add

--- src/metric_server.py
+++ src/metric_server.py
@@ -12,7 +12,7 @@ from pysnmp.hlapi import SnmpEngine, CommunityData, UsmUserData, usmHMACMD5AuthP
 from settings import log, COMMUNITY_NAME
 
 
-class Mib(object):
+class Human2(object):
     oid_map = {
         'sysName': '1.3.6.1.2.1.1.5.0',
         'ifOperStatus': '1.3.6.1.2.1.2.2.1.8',
@@ -106,10 +106,76 @@ class Mib(object):
         except StopIteration:
             log.info('Get interface list done.')
 
+class Human(object):
+    oid_map = {
+        'sysName': '1.3.6.1.2.1.1.5.0',
+        'ifOperStatus': '1.3.6.1.2.1.2.2.1.8',
+    }
+
+    def __init__(self):
+        print('Hi，我是 Human 类的初始化方法')
+        # 初始化引擎
+        self.engine = SnmpEngine()
+
+        # 选择 SNMP 协议:
+
+        # SNMP v1 和 SNMP v2c 采用团体名认证, 使用 CommunityData 类实例化
+        # SNMPv1
+        # self.communityData = CommunityData('public', mpModel=0)
+
+        # SNMPv2c
+        self.communityData = CommunityData(COMMUNITY_NAME, mpModel=1)
+
+        # SNMPv3, 则需要用户凭证, 使用 UsmUserData 类实例化, 认证和加密算法与上面设备配置相对应
+        self.userData = UsmUserData(
+            userName='admin',
+            authKey='Admin@h3c',
+            privKey='Admin@h3c',
+            authProtocol=usmHMACMD5AuthProtocol,
+            privProtocol=usmAesCfb128Protocol,
+        )
+
+        # 配置目标主机
+        # TODO
+        ip_port = ('119.131.148.169', 161)
+        self.target = UdpTransportTarget(ip_port)
+        # 实例化上下文对象
+        self.context = ContextData()
+        print(1111111111)
+
+    def get(self, metric_name):
+        log.info(f'get metric: {metric_name}')
+        # ObjectIdentity 类负责 MIB 对象的识别:
+
+        # 方法1: 指定要查询的 OID 对象或名称
+        _id = self.oid_map[metric_name]
+        oid = ObjectIdentity(id)
+
+        # 方法2: 通过oid名字查询
+        #  oid = ObjectIdentity('SNMPv2-MIB', 'sysName', 0)
+
+        # 使用 ObjectType 类初始化查询对象:
+        obj = ObjectType(oid)
+        # 使用 getCMD 方法进行查询，返回结果是一个迭代器, 需要使用 next() 来取值
+        # 传递的参数均为为上面定义的变量, 以 v2c 为例(如果是 v3，communityData 替换为 userData)
+        g = getCmd(self.engine, self.communityData, self.target, self.context, obj)
+
+        # 取值
+        errorIndication, errorStatus, errorIndex, result = next(g)
+        if str(errorStatus) != 'noError':
+            log.error(errorIndication)
+            log.error(errorStatus)
+            log.error(errorIndex)
+            return
+        # 打印输出
+        for i in result:
+            log.info(i)
+
 
 if __name__ == "__main__":
     # get sysName
-    mib = Mib()
+    mib = Human()
+    # mib = Human2()
     mib.get(metric_name='sysName')
-    log.info('============================')
-    mib.get_all(metric_name='ifOperStatus')
+    # log.info('============================')
+    # mib.get_all(metric_name='ifOperStatus')
