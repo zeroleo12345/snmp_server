6abf2f26bb3cee7564bbc80988df7ac37c2f08dc
zeroleo12345 <zeroleo12345@163.com>
2023-10-04 09:50:57 +0800

    add

--- src/metric_server.py
+++ src/metric_server.py
@@ -5,97 +5,104 @@ ac配置参考:
     https://juejin.cn/post/6988416254646157320
 """
 
-
 # 导入高层 API
 from pysnmp.hlapi import *
 
-# 初始化引擎
-engine = SnmpEngine()
 
-# 选择 SNMP 协议:
+class Mib(object):
+    oid_map = {
+        'sysName': '1.3.6.1.2.1.1.5.0',
+        'ifOperStatus': '1.3.6.1.2.1.2.2.1.8',
+    }
 
-# SNMP v1 和 SNMP v2c 采用团体名认证, 使用 CommunityData 类实例化
-# SNMPv1
-# communityData = CommunityData('public', mpModel=0)
+    def __int__(self):
+        # 初始化引擎
+        self.engine = SnmpEngine()
 
-# SNMPv2c
-communityData = CommunityData('zhoulixin', mpModel=1)
+        # 选择 SNMP 协议:
 
-# SNMPv3, 则需要用户凭证, 使用 UsmUserData 类实例化, 认证和加密算法与上面设备配置相对应
-userData = UsmUserData(
-    userName='admin',
-    authKey='Admin@h3c',
-    privKey='Admin@h3c',
-    authProtocol=usmHMACMD5AuthProtocol,
-    privProtocol=usmAesCfb128Protocol,
-)
+        # SNMP v1 和 SNMP v2c 采用团体名认证, 使用 CommunityData 类实例化
+        # SNMPv1
+        # self.communityData = CommunityData('public', mpModel=0)
 
-# 配置目标主机
-ip_port = ('119.131.148.169', 161)
-target = UdpTransportTarget(ip_port)
+        # SNMPv2c
+        self.communityData = CommunityData('zhoulixin', mpModel=1)
 
-# 实例化上下文对象
-context = ContextData()
+        # SNMPv3, 则需要用户凭证, 使用 UsmUserData 类实例化, 认证和加密算法与上面设备配置相对应
+        self.userData = UsmUserData(
+            userName='admin',
+            authKey='Admin@h3c',
+            privKey='Admin@h3c',
+            authProtocol=usmHMACMD5AuthProtocol,
+            privProtocol=usmAesCfb128Protocol,
+        )
 
+        # 配置目标主机
+        ip_port = ('119.131.148.169', 161)
+        self.target = UdpTransportTarget(ip_port)
+        # 实例化上下文对象
+        self.context = ContextData()
 
-def get_sysName(target):
-    # ObjectIdentity 类负责 MIB 对象的识别:
+    def getCmd(self, metric_name):
+        print(f'get metric: {metric_name}')
+        # ObjectIdentity 类负责 MIB 对象的识别:
 
-    # 方法1: 指定要查询的 OID 对象或名称
-    _id = '1.3.6.1.2.1.1.5.0'
-    oid = ObjectIdentity(_id)
-    
-    # 方法2: 通过oid名字查询
-    #  oid = ObjectIdentity('SNMPv2-MIB', 'sysName', 0)
+        # 方法1: 指定要查询的 OID 对象或名称
+        _id = self.oid_map[metric_name]
+        oid = ObjectIdentity(id)
 
-    # 使用 ObjectType 类初始化查询对象:
-    obj = ObjectType(oid)
-    # 使用 getCMD 方法进行查询，返回结果是一个迭代器, 需要使用 next() 来取值
-    # 传递的参数均为为上面定义的变量, 以 v2c 为例(如果是 v3，communityData 替换为 userData)
-    g = getCmd(engine, communityData, target, context, obj)
+        # 方法2: 通过oid名字查询
+        #  oid = ObjectIdentity('SNMPv2-MIB', 'sysName', 0)
 
-    # 取值
-    errorIndication, errorStatus, errorIndex, result = next(g)
+        # 使用 ObjectType 类初始化查询对象:
+        obj = ObjectType(oid)
+        # 使用 getCMD 方法进行查询，返回结果是一个迭代器, 需要使用 next() 来取值
+        # 传递的参数均为为上面定义的变量, 以 v2c 为例(如果是 v3，communityData 替换为 userData)
+        g = getCmd(self.engine, self.communityData, self.target, context, obj)
 
-    # 打印输出
-    for i in result:
-        print(i)
+        # 取值
+        errorIndication, errorStatus, errorIndex, result = next(g)
 
+        # 打印输出
+        for i in result:
+            print(i)
 
-def get_ifOperStatus(target):
-    """
-    这个函数是查询接口列表, 和上面查询 sysName 的区别是使用了 nextCmd 来获取一个 MIB 子树的全部内容
-    主要是 `lexicographicMode=False` 参数, 默认为 `True`, 会一直查询到 MIB 树结束.
-    """
-    # 方法1: 指定要查询的 OID 对象或名称
-    _id = '1.3.6.1.2.1.2.2.1.8'
-    oid = ObjectIdentity(_id)
+    def nextCmd(self, metric_name):
+        """
+        这个函数是查询接口列表, 和上面查询 sysName 的区别是使用了 nextCmd 来获取一个 MIB 子树的全部内容
+        主要是 `lexicographicMode=False` 参数, 默认为 `True`, 会一直查询到 MIB 树结束.
+        """
+        print(f'get metric: {metric_name}')
+        # 方法1: 指定要查询的 OID 对象或名称
+        _id = self.oid_map[metric_name]
+        oid = ObjectIdentity(id)
 
-    # 方法2: 通过oid名字查询
-    #  oid = ObjectIdentity('SNMPv2-MIB', 'ifDescr')
+        # 方法2: 通过oid名字查询
+        #  oid = ObjectIdentity('SNMPv2-MIB', 'ifDescr')
 
-    # 使用 ObjectType 类初始化查询对象:
-    obj = ObjectType(oid)
-    # 传递的参数均为为上面定义的变量, 以 v2c 为例(如果是 v3，communityData 替换为 userData)
-    g = nextCmd(engine, communityData, target, context, obj, lexicographicMode=False)
-    # 手动迭代并输出内容，并进行迭代器终止的判断
-    try:
-        while True:
-            errorIndication, errorStatus, errorIndex, varBinds = next(g)
-            #  (Pdb) errorStatus.namedValues.getName('noError')
-            #  (Pdb) errorStatus.namedValues.getName(0)
-            if str(errorStatus) != 'noError':
-                print(errorIndication)
-                print(errorStatus)
-                print(errorIndex)
-                continue
-            for iface in varBinds:
-                print(iface)
-    except StopIteration:
-        print('Get interface list done.')
+        # 使用 ObjectType 类初始化查询对象:
+        obj = ObjectType(oid)
+        # 传递的参数均为为上面定义的变量, 以 v2c 为例(如果是 v3，communityData 替换为 userData)
+        g = nextCmd(self.engine, self.communityData, self.target, context, obj, lexicographicMode=False)
+        # 手动迭代并输出内容，并进行迭代器终止的判断
+        try:
+            while True:
+                errorIndication, errorStatus, errorIndex, varBinds = next(g)
+                #  (Pdb) errorStatus.namedValues.getName('noError')
+                #  (Pdb) errorStatus.namedValues.getName(0)
+                if str(errorStatus) != 'noError':
+                    print(errorIndication)
+                    print(errorStatus)
+                    print(errorIndex)
+                    continue
+                for iface in varBinds:
+                    print(iface)
+        except StopIteration:
+            print('Get interface list done.')
 
 
 if __name__ == "__main__":
-    get_sysName(target)
+    # get sysName
+    getCmd(metric_name='sysName')
     print('============================')
-    get_ifOperStatus(target)
+    nextCmd(metric_name='ifOperStatus')
